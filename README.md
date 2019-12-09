14af657d0d9d48798dc6aee4b1b7b634e6c8bd45
xiongsongsong <xiongsongsong@jd.com>
2019-12-09 10:41:27 +0800

    开发配置完成

---
+++ README.md
@@ -0,0 +1,4 @@
+# React-SPA-template
+
+React单页模板
+使用 webpack4 + babel7 + eslint
--- src/components/CommsA/CommsA.jsx
+++ src/components/CommsA/CommsA.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import './CommsA.scss'
 
-const CommsA = () => (<div className="commsa">This is component A!</div>)
+const CommsA = () => (<div className="commsa">This is component A! I am coming</div>)
 
 export default CommsA
